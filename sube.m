8bdd7a6899720f297eec46555497a3bc5c12cdc9
liujiawen2018phyche <97917263+liujiawen2018phyche@users.noreply.github.com>
2023-04-07 05:15:12 +0800

    Add files via upload

---
+++ sube.m
@@ -0,0 +1,11 @@
+start test url_server {
+ping //url_server/
+if ans = Y then send "Server Normal";
+other then send "Server ERROR";
+};
+start test UI {
+if UI MODEL = Y then send "Server Normal";
+other then send "UI ERROR";
+end process;
+end all;
+end;
