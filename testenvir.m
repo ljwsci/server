18a8b4790a0181c342bb1d1a4165713db2dfe801
liujiawen2018phyche <97917263+liujiawen2018phyche@users.noreply.github.com>
2024-02-02 03:04:31 +0800

    Add files via upload

---
+++ testenvir.m
@@ -0,0 +1,16 @@
+use Theia IDE.iso
+setup .> linux
+set VSCode
+set
+Go
+set Python
+set Node.js
+set Clang
+set OpenJDK
+end process;
+use LAMP
+use Linux./etc
+set Apache
+set MySQL
+set PHP
+end process;
